29837c8866bca061669acf14502ccef63ee3b24c
Luiza Santos <luiza.marques_@hotmail.com>
2023-08-28 11:28:51 -0300

    updated notebook and models

--- dev/luizamfsantos/iris_data/train_test.py
+++ dev/luizamfsantos/iris_data/train_test.py
@@ -1,2 +1,27 @@
 import pandas as pd
 import numpy as np
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.naive_bayes import CategoricalNB
+
+### Model #1 Decision Tree
+def decision_tree(train_X, val_X, train_y, val_y):
+    classifier = DecisionTreeClassifier(random_state=1)
+    classifier.fit(train_X,train_y)
+    predictions = classifier.predict(val_X)
+    return mean_absolute_error(predictions, val_y)
+
+### Model #2 Random Forest
+def forest(train_X, val_X, train_y, val_y):
+    classifier = RandomForestRegressor(random_state=1)
+    classifier.fit(train_X,train_y)
+    predictions = classifier.predict(val_X)
+    return mean_absolute_error(predictions, val_y)
+
+### Model #3 Categorical Naive Bayes
+def bayes(train_X, val_X, train_y, val_y):
+    classifier = CategoricalNB(force_alpha=True)
+    classifier.fit(train_X,train_y)
+    predictions = classifier.predict(val_X)
+    return mean_absolute_error(predictions, val_y)
